cedb0e3252cb9667c896c3e88f5a8876d85a739c
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-05-15 20:09:31 +0330

    edit: moved all of thhe 'infonon-code files to database directory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dependencies.log import Log
-from dependencies.parser import doc_parser, query_parser
+from dependencies.document_parser import doc_parser
+from dependencies.query_resolver import query_parser
 from dependencies.indexer import Indexer
 from dependencies.wildcard import KgramIndexer
 
@@ -8,34 +9,36 @@ class Main:
     dict = {}
     wildcard_dict = {}
     paths = {
-        "document": "documents",
-        "posting": "posting list",
+        "document": "Database/Documents",
+        "log": "Database/Logs",
+        "posting": "Database/Posting List",
+        "dict": "Database/Dictionary",
         # the path of the log files is created in the log.py
     }
 
     def __init__(self):
         # initializing logging system
-        self.log = Log().log
+        self.log = Log(self.paths["log"]).log
 
         # pre-process documents
         tokens = doc_parser(self.paths["document"], self.log)
 
         # index documents
-        indxr = Indexer(tokens, self.paths["posting"], self.log)
+        indxr = Indexer(tokens, self.paths["posting"], self.paths["dict"], self.log)
         self.dict = indxr.dict
 
+        # # create wildcard matcher
+        # matcher = Indexer(tokens, 1, self.log)
+        # self.dict = indxr.dict
+
         # wildcard indexer
-        wildcard = KgramIndexer(self.dict)
+        wildcard = KgramIndexer(self.dict, self.paths["dict"], self.log)
         self.wildcard_dict = wildcard.dict
 
-        # while True:
-        #     (input("Please enter your query: "))
-        #     query_parser()
-
-        #     self.rank_results()
-
-    def rank_results(self):
-        self.log("Started to execute rank_results")
+        while True:
+            query = input("Please enter your query: ")
+            results = query_parser(query)
+            print("results are: ", results)
 
 
 Main()
